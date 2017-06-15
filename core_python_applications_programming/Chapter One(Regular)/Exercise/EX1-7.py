19c1331cae95a4f9aa479b4d2ea08b16f472bf68
boaass <boaass@sina.com>
2017-06-15 22:43:11 +0800

    EX1-7.PY add log

--- core_python_applications_programming/Chapter One(Regular)/Exercise/EX1-7.py
+++ core_python_applications_programming/Chapter One(Regular)/Exercise/EX1-7.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# 匹配能够便是 Python 整数的字符串集
+# 匹配能够表示 Python 整数的字符串集
 
 import re
 
@@ -7,6 +7,7 @@ data = ('11111111111111', '22222222222+2222222', '333333333.33333')
 
 
 def match_int_type_number(num):
+    # 还要根据系统判断 32位或64位最大最小值，此处暂不做处理
     m = re.match(r'[+-]?\d+$', num)
     if m is not None:
         return m.group()
