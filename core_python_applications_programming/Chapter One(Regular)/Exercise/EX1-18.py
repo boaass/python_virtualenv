7810299ed23cb76dbdf7be7a301bf792e23b8ab7
boaass <boaass@sina.com>
2017-06-19 23:57:42 +0800

    fix match multiple space

--- core_python_applications_programming/Chapter One(Regular)/Exercise/EX1-18.py
+++ core_python_applications_programming/Chapter One(Regular)/Exercise/EX1-18.py
@@ -7,9 +7,9 @@ import re
 with open('redata.txt', 'r') as f:
     for line_data in f:
         m = re.match(
-            r'^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{1,2}\s\d{2}\:\d{2}\:\d{2}\s\w+',
+            r'^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s{1,2}\d{1,2}\s\d{2}\:\d{2}\:\d{2}\s\w+',
             line_data)
         if m is not None:
             print 'data integrity!'
         else:
-            print 'data corruption!'
+            print 'data corruption! data:%s' % line_data
