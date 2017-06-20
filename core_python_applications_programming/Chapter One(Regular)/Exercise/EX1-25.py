46bbf95dd095a75b37ad9af4beb76aec3d9520f9
boaass <boaass@sina.com>
2017-06-20 21:37:42 +0800

    1.add EX1-25.py

---
+++ core_python_applications_programming/Chapter One(Regular)/Exercise/EX1-25.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# 从电子邮箱中提取登录名和域名（包括主域名和高级域名）
+
+import re
+
+with open('redata.txt', 'r') as f:
+    for line_data in f:
+        m = re.match(r'.*::(\w{4,8})@(\w+)\.(\w+)::.*', line_data)
+        if m is not None:
+            print m.groups()
+
+f.close()
